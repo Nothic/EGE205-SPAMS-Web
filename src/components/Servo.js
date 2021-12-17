57b6fd89007a5988d32914cc3ed2e7cf4f38de07
Nothic <noth.tanyh@gmail.com>
2021-12-17 19:10:04 +0800

    pinging spams-server button added

--- src/components/Servo.js
+++ src/components/Servo.js
@@ -1,11 +1,26 @@
 import ToggleSwitch from "./ToggleSwitch";
+import socketio, { io } from "socket.io-client";
 
-function Servo(){
-    return(
-        <div className="bg-emerald-100 rounded-b-md">
-        <ToggleSwitch/>
-        </div>
-    );
+function pingServo() {
+  var socket = io("http://localhost:3001/");
+  var buttonServo = document.getElementById('ping servo')
+  console.log("Button pressed");
+
+//   form.addEventListener('')
+}
+
+function Servo() {
+  return (
+    <div className="bg-emerald-100 rounded-b-md">
+      <ToggleSwitch />
+      <button id='ping servo'
+        onClick={pingServo()}
+        className="bg-emerald-500 rounded-md m-1 p-1"
+      >
+        Ping Server
+      </button>
+    </div>
+  );
 }
 
 export default Servo;
